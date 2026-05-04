ea844557e3eace8da8f1610623205b661478b196
迫田裕介 <sakodayuusuke@sakotayuusukenoMacBook-Air.local>
2026-05-04 22:19:09 +0900

    feat: url.pyの作成 views.pyに仮のクラスを作成 allauthの設定を追加

--- tycom/urls.py
+++ tycom/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "tycom"
 
 urlpatterns = [
-    path("stage-select/", views.stage_select, name="stage_select"),
-    path("game/play/", views.game_play, name="game_play"),
-    path("game/time_end/", views.game_time_end, name="game_time_end"),
-    path("game/result/", views.game_result, name="game_result"),
+    path("stage-select/", views.StageSelectView.as_view(), name="stage_select"),
+    path("game/play/", views.GamePlayView.as_view(), name="game_play"),
+    path("game/time-end/", views.GameTimeEndView.as_view(), name="game_time_end"),
+    path("game/result/", views.GameResultView.as_view(), name="game_result"),
 ]
--- tycom/views.py
+++ tycom/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+class StageSelectView(View):
+    def get(self, request):
+        return HttpResponse("ステージ選択画面")
+
+
+class GamePlayView(View):
+    def get(self, request):
+        return HttpResponse("問題画面")
+
+
+class GameTimeEndView(View):
+    def post(self, request):
+        return HttpResponse("時間切れ")
+
+
+class GameResultView(View):
+    def get(self, request):
+        return HttpResponse("結果表示")
