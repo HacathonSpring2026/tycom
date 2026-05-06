cc521648877118f1b9e4c61e78ecbcfde5a3663d
迫田裕介 <sakodayuusuke@sakotayuusukenoMacBook-Air.local>
2026-05-06 10:19:30 +0900

    fix: idの削除,CustomUser修,・不要ファイルをgit管理から外す

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
     """拡張ユーザーモデル"""
+
+    first_name = None
+    last_name = None
+
+    username = models.CharField(max_length=150, unique=False)
+
+    email = models.EmailField(unique=True)
 
     class Meta:
         verbose_name_plural = "CustomUser"
